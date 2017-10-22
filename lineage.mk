e1d21b89ec577f67c6b35d6e2a25e78824f16c68
Danila Suslov <assusdan@suslovd.ru>
2017-10-22 14:14:12 +0300

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,6 @@
 # Boot animation
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 480
+TARGET_SCREEN_HEIGHT := 854
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
